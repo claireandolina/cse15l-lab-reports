b7e93be203845c4f10c329f6022aeb1455a5fee0
claireandolina <108210076+claireandolina@users.noreply.github.com>
2023-11-02 23:42:30 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,32 +1,9 @@
-/*
-In your Edstem workspace, make a new file called SearchEngine.java. In it, 
-implement a web server (like NumberServer.java) that tracks a list of strings.
-It should support a path for
-1. adding a new string to the list, and a path for
-2. querying the list of strings and returning a list of all strings that have a given substring.
-*/
-
 import java.io.IOException;
 import java.net.*;
 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
     int num = 0;
-    //ArrayList str = new ArrayList();
     StringBuilder str = new StringBuilder();
-
-
-/*
-/add?s=anewstringtoadd
-
-/add?s=pineapple
-
-/add?s=apple
-
-/search?s=app
-(would return pineapple and apple)
-*/
 
     public String handleRequest(URI url) {
         if(url.getQuery()==null)
@@ -40,23 +17,11 @@ class Handler implements URLHandler {
                     num++;
                     str.append(num+". "+parameters[1]+"\n");
                     return str.toString();
-                    //"'%s' is now added to the master string! It's now %d", parameters[1], str);
                 }
                 else{
                     return "Invalid query!";
                 }
             }
-                /*
-            else if (url.getPath().contains("/search")) {
-                StringBuilder queryResult = new StringBuilder();
-                for (string value : str) {
-                    if(value.contains(parameters[1]))
-                    {
-                        queryResult.append(value +", ");
-                    }
-                }
-                return queryResult.toString();
-            }*/
             else{
                 return "Invalid query!";
             }
